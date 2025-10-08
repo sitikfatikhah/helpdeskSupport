15fd934c7ed98909797f9388ff231f77bfefc958
sitikfatikhah <khamidatul94wnsb@gmail.com>
2025-10-08 10:50:20 +0700

    update link web ke http://localhost:8000/app

--- app/Providers/Filament/AdminPanelProvider.php
+++ app/Providers/Filament/AdminPanelProvider.php
@@ -33,7 +33,7 @@ class AdminPanelProvider extends PanelProvider
         return $panel
             ->default()
             ->id('admin')
-            ->path('admin')
+            ->path('app') //minta diganti jadi app
             ->brandName('IT Helpdesk')
             // ->brandLogo(asset('images/logo.png'))
             // ->favicon(asset('images/favicon.ico'))
